f11d75b0bc1df8f241cd0044cd9b4ab5856b2626
Marat Tuktarov <mtuktarov@yahoo.com>
2019-12-28 09:40:59 +0300

    changes

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 read_link(){
-    target_file=$(which $0)
+    target_file=$(which ./$0)
     cd `dirname $target_file`
     target_file=`basename $target_file`
 
@@ -12,17 +12,15 @@ read_link(){
         target_file=`basename $target_file`
     done
 
-    # Compute the canonicalized name by finding the physical path
     # for the directory we're in and appending the target file.
     phys_dir=`pwd -P`
     result=$phys_dir/$target_file
     echo $result
 }
-
-my_location=$(read_link $(which $0))
+my_location=$(read_link)
 my_dir="${my_location%/*}"
 
-if [[ $(uname) == "Linux" ]] ; then
+if [[ $(uname) == 'Linux' ]] ; then
     if [ -f ${HOME}/.bashrc ] ; then
         mv ${HOME}/.bashrc ${my_dir}/.bashrc_old
     fi
